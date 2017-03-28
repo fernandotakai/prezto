94956c6eae6a0247508c9eca8a3ee3d8a0572ec3
Fernando Takai <fernando.takai@gmail.com>
2017-03-28 13:28:13 -0300

    morestuff

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -22,7 +22,7 @@ _ssh_agent_sock="${TMPDIR:-/tmp}/ssh-agent.sock"
 # Start ssh-agent if not started.
 if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   # Export environment variables.
-  source "$_ssh_agent_env"
+  source "$_ssh_agent_env" 2> /dev/null
 
   # Start ssh-agent if not started.
   if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
